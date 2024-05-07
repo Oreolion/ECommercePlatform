b42aaf0ac86c8b5e0b074b17c494ea4d983a1195
oreolion <remyoreo11@gmail.com>
2024-05-07 11:52:34 -0700

    add navigation transition;

--- src/App.jsx
+++ src/App.jsx
@@ -11,35 +11,42 @@ import Cart from "./pages/Cart";
 import NotFound from "./components/NotFound";
 import { CartContextProvider } from "./components/CartContext.jsx";
 import { UserContextProvider } from "./components/UserContext.jsx";
-import {ProtectedRoute} from "./utils/ProtectedRoute.jsx";
+import { ProtectedRoute } from "./utils/ProtectedRoute.jsx";
+import { SwitchTransition, CSSTransition } from "react-transition-group";
+import { useLocation } from "react-router-dom";
 
 function App() {
+    const location = useLocation()
   return (
     <>
       <CartContextProvider>
         <UserContextProvider>
-          <Routes>
-            <Route path="/" element={<Navbar />}>
-              <Route exact path="/" element={<Homepage />}></Route>
-              <Route path="/about" element={<AboutUs />}></Route>
-              <Route path="/shop" element={<Shop />}></Route>
-              <Route path="/contact" element={<ContactUs />}></Route>
-              <Route path="/cart" element={<Cart />}></Route>
-              <Route
-              path="/checkout"
-              element={
-                <ProtectedRoute>
-                  <Checkout />
-                </ProtectedRoute>
-              }
-            ></Route>
-            </Route>
-            <Route path="/signup" element={<SignUp />}></Route>
-            <Route path="/login" element={<Login />}></Route>
-           
-            <Route path="*" element={<NotFound />}></Route>
-            <Route path="/*" element={<NotFound />}></Route>
-          </Routes>
+          <SwitchTransition component={null}>
+            <CSSTransition key={location.pathname} classNames='fade' timeout={300} unmountOnExit>
+              <Routes location={location}>
+                <Route path="/" element={<Navbar />}>
+                  <Route exact path="/" element={<Homepage />}></Route>
+                  <Route path="/about" element={<AboutUs />}></Route>
+                  <Route path="/shop" element={<Shop />}></Route>
+                  <Route path="/contact" element={<ContactUs />}></Route>
+                  <Route path="/cart" element={<Cart />}></Route>
+                  <Route
+                    path="/checkout"
+                    element={
+                      <ProtectedRoute>
+                        <Checkout />
+                      </ProtectedRoute>
+                    }
+                  ></Route>
+                </Route>
+                <Route path="/signup" element={<SignUp />}></Route>
+                <Route path="/login" element={<Login />}></Route>
+
+                <Route path="*" element={<NotFound />}></Route>
+                <Route path="/*" element={<NotFound />}></Route>
+              </Routes>
+            </CSSTransition>
+          </SwitchTransition>
         </UserContextProvider>
       </CartContextProvider>
     </>
